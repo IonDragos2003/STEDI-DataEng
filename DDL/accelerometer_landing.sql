ae782c77d733ff9183956c4284bc193cbd8ffe46
Dragos-Georgian Ion <iondragos2003@gmail.com>
2024-06-22 13:27:10 +0100

    Update accelerometer_landing.sql

--- DDL/accelerometer_landing.sql
+++ DDL/accelerometer_landing.sql
@@ -1,17 +1,22 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
-  `user` string,
-  `timeStamp` bigint,
-  `x` float,
-  `y` float,
-  `z` float
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-  'ignore.malformed.json' = 'FALSE',
-  'dots.in.keys' = 'FALSE',
-  'case.insensitive' = 'TRUE',
-  'mapping' = 'TRUE'
-)
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://stedi-dragos/accelerometer/landing/'
-TBLPROPERTIES ('classification' = 'json');
+CREATE EXTERNAL TABLE `accelerator_landing`(
+  `user` string COMMENT 'from deserializer', 
+  `x` float COMMENT 'from deserializer', 
+  `y` float COMMENT 'from deserializer', 
+  `z` float COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://dragos-stedi/accelerometer/landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1719059198')
