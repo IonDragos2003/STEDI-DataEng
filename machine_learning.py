703a70485024b55ece1fbe219acc6b1e03cfe7b1
Dragos-Georgian Ion <iondragos2003@gmail.com>
2024-06-23 14:27:07 +0100

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -4,16 +4,13 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
-from pyspark.sql import functions as SqlFuncs
+from awsglue import DynamicFrame
 
-def sparkAggregate(glueContext, parentFrame, groups, aggs, transformation_ctx) -> DynamicFrame:
-    aggsFuncs = []
-    for column, func in aggs:
-        aggsFuncs.append(getattr(SqlFuncs, func)(column))
-    result = parentFrame.toDF().groupBy(*groups).agg(*aggsFuncs) if len(groups) > 0 else parentFrame.toDF().agg(*aggsFuncs)
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
     return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
-
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -21,32 +18,57 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node Step Trainer Trusted
-StepTrainerTrusted_node1719062686505 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/step_trainer/trusted/"], "recurse": True}, transformation_ctx="StepTrainerTrusted_node1719062686505")
+# Script generated for node AC Trusted
+ACTrusted_node1719147920031 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/accelerometer/trusted/"], "recurse": True}, transformation_ctx="ACTrusted_node1719147920031")
 
-# Script generated for node Accelerometer Trusted
-AccelerometerTrusted_node1719062686968 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/accelerometer/trusted/"], "recurse": True}, transformation_ctx="AccelerometerTrusted_node1719062686968")
+# Script generated for node ST Trusted
+STTrusted_node1719147919650 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/step_trainer/trusted/"], "recurse": True}, transformation_ctx="STTrusted_node1719147919650")
 
-# Script generated for node Customer Curated
-CustomerCurated_node1719062687477 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/customer/curated/"], "recurse": True}, transformation_ctx="CustomerCurated_node1719062687477")
-
-# Script generated for node Join Acc Cust
-JoinAccCust_node1719062863201 = Join.apply(frame1=AccelerometerTrusted_node1719062686968, frame2=CustomerCurated_node1719062687477, keys1=["user"], keys2=["email"], transformation_ctx="JoinAccCust_node1719062863201")
-
-# Script generated for node Join prevJoin StepTrainer
-JoinAccCust_node1719062863201DF = JoinAccCust_node1719062863201.toDF()
-StepTrainerTrusted_node1719062686505DF = StepTrainerTrusted_node1719062686505.toDF()
-JoinprevJoinStepTrainer_node1719062929086 = DynamicFrame.fromDF(JoinAccCust_node1719062863201DF.join(StepTrainerTrusted_node1719062686505DF, (JoinAccCust_node1719062863201DF['serialnumber'] == StepTrainerTrusted_node1719062686505DF['serialNumber']), "left"), glueContext, "JoinprevJoinStepTrainer_node1719062929086")
+# Script generated for node Cust Curated
+CustCurated_node1719147920228 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://dragos-stedi/customer/curated/"], "recurse": True}, transformation_ctx="CustCurated_node1719147920228")
 
 # Script generated for node Change Schema
-ChangeSchema_node1719063780161 = ApplyMapping.apply(frame=JoinprevJoinStepTrainer_node1719062929086, mappings=[("z", "double", "z", "double"), ("user", "string", "user", "string"), ("y", "double", "y", "double"), ("x", "double", "x", "double"), ("timestamp", "bigint", "timestamp", "bigint"), ("customername", "string", "customername", "string"), ("email", "string", "email", "string"), ("phone", "string", "phone", "string"), ("birthday", "string", "birthday", "string"), ("serialnumber", "string", "serialNumber_prevJoin", "string"), ("registrationdate", "bigint", "registrationdate", "bigint"), ("lastupdatedate", "bigint", "lastupdatedate", "bigint"), ("sharewithresearchasofdate", "bigint", "sharewithresearchasofdate", "bigint"), ("sharewithpublicasofdate", "bigint", "sharewithpublicasofdate", "bigint"), ("sharewithfriendsasofdate", "bigint", "sharewithfriendsasofdate", "bigint"), ("sensorReadingTime", "bigint", "sensorReadingTime", "bigint"), ("serialNumber", "string", "serialNumber", "string"), ("distanceFromObject", "int", "distanceFromObject", "int")], transformation_ctx="ChangeSchema_node1719063780161")
+ChangeSchema_node1719148618600 = ApplyMapping.apply(frame=STTrusted_node1719147919650, mappings=[("sensorreadingtime", "long", "st_sensorreadingtime", "long"), ("serialnumber", "string", "st_serialnumber", "string"), ("distancefromobject", "int", "st_distancefromobject", "int")], transformation_ctx="ChangeSchema_node1719148618600")
 
-# Script generated for node Aggregate
-Aggregate_node1719063069340 = sparkAggregate(glueContext, parentFrame = ChangeSchema_node1719063780161, groups = ["x", "y", "z", "user", "sensorReadingTime", "serialNumber", "distanceFromObject", "serialNumber_prevJoin"], aggs = [["timestamp", "count"]], transformation_ctx = "Aggregate_node1719063069340")
+# Script generated for node SQL Query
+SqlQuery109 = '''
+select * from customer c join accelerometer a on c.email = a.user
+'''
+SQLQuery_node1719147997327 = sparkSqlQuery(glueContext, query = SqlQuery109, mapping = {"customer":CustCurated_node1719147920228, "accelerometer":ACTrusted_node1719147920031}, transformation_ctx = "SQLQuery_node1719147997327")
+
+# Script generated for node SQL Query
+SqlQuery108 = '''
+select * from stepTrainer s join joinedPrev j on s.st_serialnumber = j.serialnumber
+'''
+SQLQuery_node1719148110745 = sparkSqlQuery(glueContext, query = SqlQuery108, mapping = {"joinedPrev":SQLQuery_node1719147997327, "stepTrainer":ChangeSchema_node1719148618600}, transformation_ctx = "SQLQuery_node1719148110745")
+
+# Script generated for node SQL Query
+SqlQuery110 = '''
+SELECT
+    y,
+    z,
+    x,
+    user,
+    st_sensorReadingTime,
+    st_distanceFromObject,
+    serialNumber,
+    COUNT(timeStamp) as timeStamp_count
+FROM
+    myDataSource
+GROUP BY
+    y,
+    z,
+    x,
+    user,
+    st_sensorReadingTime,
+    st_distanceFromObject,
+    serialNumber
+'''
+SQLQuery_node1719148503915 = sparkSqlQuery(glueContext, query = SqlQuery110, mapping = {"myDataSource":SQLQuery_node1719148110745}, transformation_ctx = "SQLQuery_node1719148503915")
 
 # Script generated for node Amazon S3
-AmazonS3_node1719063968586 = glueContext.getSink(path="s3://dragos-stedi/machine_learning/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], compression="", enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1719063968586")
-AmazonS3_node1719063968586.setCatalogInfo(catalogDatabase="stedi",catalogTableName="machine_learning")
-AmazonS3_node1719063968586.setFormat("json")
-AmazonS3_node1719063968586.writeFrame(Aggregate_node1719063069340)
+AmazonS3_node1719148738794 = glueContext.getSink(path="s3://dragos-stedi/ml_new/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1719148738794")
+AmazonS3_node1719148738794.setCatalogInfo(catalogDatabase="stedi",catalogTableName="ml_new")
+AmazonS3_node1719148738794.setFormat("json")
+AmazonS3_node1719148738794.writeFrame(SQLQuery_node1719148503915)
 job.commit()
